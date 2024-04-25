f98c88fca8547f0f0bf8014febe65f73e5d72874
Frédéric Godard <godard.fred@gmail.com>
2024-04-25 17:35:10 +0200

    implémentation des tests java

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,10 +1,19 @@
 package com.openclassrooms.starterjwt;
 
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.services.TeacherService;
+import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
+
+	@Autowired
+	TeacherService teacherService;
 
 	@Test
 	public void contextLoads() {
